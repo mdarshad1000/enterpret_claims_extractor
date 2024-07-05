99c235371160d95aec135fd9e19fbc6a31a67bf6
Mohammad Arshad <mdarshad1000@gmail.com>
2024-07-05 08:23:58 +0530

    .

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="enterpret_claims_extractor",
-    version="0.1.0",
+    version="0.1.1",
     author="Mohd Arshad",
     author_email="mdarshad1000@gmail.com",
     description="A tool for extracting claims from various types of records",
